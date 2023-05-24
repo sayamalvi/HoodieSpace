d7d2031fadf25a5f7b765d710fc0fce98280b444
Sayam Alvi <sayamalvi07@gmail.com>
2023-05-24 16:52:19 +0530

    feat: added random products on homepage shopping list

--- client/src/components/Hoodies.jsx
+++ client/src/components/Hoodies.jsx
@@ -10,10 +10,14 @@ const Hoodies = () => {
   const hoodies = items.filter(
     (item) => item.attributes.category === "hoodies"
   );
+  hoodies.map((item) => console.log(item.name));
   async function getItems() {
-    const items = await fetch("http://localhost:1337/api/items?populate=*&pagination[start]=0&pagination[limit]=-1", {
-      method: "GET",
-    });
+    const items = await fetch(
+      "http://localhost:1337/api/items?populate=*&pagination[start]=0&pagination[limit]=-1",
+      {
+        method: "GET",
+      }
+    );
     const itemsJSON = await items.json();
 
     dispatch(setItems(itemsJSON.data));
--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -7,35 +7,56 @@ import { useDispatch, useSelector } from "react-redux";
 const ShoppingList = () => {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.cart.items);
+  const [randomItems, setRandomItems] = useState(new Set());
 
-  // grab all the items from strapi backend
   async function getItems() {
     const perPage = 44;
     const items = await fetch(
-      `http://localhost:1337/api/items?populate=*&pagination[start]=0&pagination[limit]=-1`,
+      `http://localhost:1337/api/items?populate=image&pagination[start]=0&pagination[limit]=-1`,
       {
         method: "GET",
       }
     );
     const itemsJSON = await items.json();
-
-    //populate items array
     dispatch(setItems(itemsJSON.data));
-    {
-      console.log(itemsJSON);
-    }
   }
 
   useEffect(() => {
-    // call the strapi api
     getItems();
   }, []);
+
+  useEffect(() => {
+    // Generate random items
+    const generateRandomItems = () => {
+      const newRandomItems = new Set();
+      while (newRandomItems.size < 6 && newRandomItems.size < items.length) {
+        const randomIndex = Math.floor(Math.random() * items.length);
+        newRandomItems.add(items[randomIndex]);
+      }
+      setRandomItems(newRandomItems);
+    };
+
+    generateRandomItems();
+  }, [items]);
 
   return (
     <Box width="80%" margin="50px auto">
       <Typography textAlign="center" fontSize="25px">
         FEATURED
       </Typography>
+      <Box
+        margin="0 auto"
+        display="grid"
+        gridTemplateColumns="repeat(auto-fill, 300px)"
+        justifyContent="space-around"
+        rowGap="20px"
+        columnGap="1.33%"
+      >
+        {/* Display random items */}
+        {Array.from(randomItems).map((item, index) => (
+          <Item key={index} item={item} />
+        ))}
+      </Box>
     </Box>
   );
 };
